0570221dd0c02ffba6e2c066daff6609ec11346a
daphne <daphneyang0127@gmail.com>
2023-11-30 23:16:49 +0800

    linebot with ASR

--- linebot/main.py
+++ linebot/main.py
@@ -2,10 +2,10 @@ from dotenv import load_dotenv
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
-                            ImageSendMessage, AudioMessage)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,AudioMessage)
 import os
-import uuid
+import openai
+from openai import OpenAI
 
 load_dotenv()
 print(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
@@ -19,70 +19,86 @@ handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 # Initialize the bot
 @app.route("/callback", methods=['POST'])
 def callback():
-  signature = request.headers['X-Line-Signature']
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    print(
-        "Invalid signature. Please check your channel access token/channel secret."
-    )
-    abort(400)
-  return 'OK'
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        abort(400)
+    return 'OK'
 
 
 # Text message events
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-  print("enter txt msg event handler")
+    print("enter txt msg event handler")
 
-  user_id = event.source.user_id
-  text = event.message.text.strip()
-  # print("------" + event.reply_token + "------")
-  # print("------" + text + "------")
-  try:
-    msg = TextSendMessage(text=event.message.text)
-  except Exception as e:
-    msg = TextSendMessage(text=str(e))
-  try:
-    line_bot_api.reply_message(event.reply_token, msg)
-  except Exception as e:
-    print("error" + str(e))
+    user_id = event.source.user_id
+    text = event.message.text.strip()
+    try:
+        msg = TextSendMessage(text=event.message.text)
+    except Exception as e:
+        msg = TextSendMessage(text=str(e))
+    try:
+        line_bot_api.reply_message(event.reply_token, msg)
+    except Exception as e:
+        print("error" + str(e))
 
 
 # Text message events
 @handler.add(MessageEvent, message=AudioMessage)
 def handle_audio_message(event):
-  user_id = event.source.user_id
+    user_id = event.source.user_id
 
-  msg_content = line_bot_api.get_message_content(event.message.id)
-  # audio_data = b''
-  # for chunk in msg_content.iter_content():
-  #   audio_data += chunk
-  # print("--------" + str(audio_data) + "----------")
-  # bucket_name = "testing_bucket"
-  # upload_blob_from_memory(bucket_name, audio_data, f'{user_id}.mp3')
+    msg_content = line_bot_api.get_message_content(event.message.id)
 
-  # file_name = user_id+'.mp3'
-  path='/Users/daphne/desktop'
-  with open('OutputFile.mp3', 'wb') as fd:
-      for chunk in msg_content.iter_content():
-          fd.write(chunk)
-        
-  #reply message
-  msg = "Audio message successfully received!"
-  try:
+    file_name = user_id + '.m4a'
+    with open(file_name, 'wb') as fd:
+        for chunk in msg_content.iter_content():
+            fd.write(chunk)
+
+    #reply message
+    msg = "Audio message successfully received!"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
-  except Exception as e:
-    print("error" + str(e))
+  
+    try:
+        openai.api_key = os.environ['OPENAI_API_KEY']
+
+    except KeyError:
+        sys.stderr.write("""
+        You haven't set up your API key yet.
+
+        If you don't have an API key yet, visit:
+
+        https://platform.openai.com/signup
+
+        1. Make an account or sign in
+        2. Click "View API Keys" from the top right menu.
+        3. Click "Create new secret key"
+
+        Then, open the Secrets Tool and add OPENAI_API_KEY as a secret.
+        """)
+        exit(1)
+
+    client = OpenAI()
+        audio_file = open(file_name, "rb")
+        transcript = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+    )
+    print("successfully generate transcript!")
+    print(transcript)
 
 
 @app.route("/", methods=['GET'])
 def home():
-  return "hi"
+    return "hi"
 
 
 if __name__ == "__main__":
-  # app.run()
-  app.run(host='0.0.0.0', port=80)
+    # app.run()
+    app.run(host='0.0.0.0', port=80)
