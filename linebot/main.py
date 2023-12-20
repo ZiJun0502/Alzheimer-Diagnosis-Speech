0e5847541d91654342a85c88fd2c718a41c25ddc
daphne <daphneyang0127@gmail.com>
2023-12-20 19:49:58 +0800

    save as csv

--- linebot/main.py
+++ linebot/main.py
@@ -29,66 +29,66 @@ handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 # Initialize the bot
 @app.route("/callback", methods=['POST'])
 def callback():
-  signature = request.headers['X-Line-Signature']
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    print(
-        "Invalid signature. Please check your channel access token/channel secret."
-    )
-    abort(400)
-  return 'OK'
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        abort(400)
+    return 'OK'
 
 
 # Text message events
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
 
-  user_id = event.source.user_id
-  text = "I can only receive audio message, please send an audio message around 30 sec"
+    user_id = event.source.user_id
+    text = "I can only receive audio message, please send an audio message around 30 sec"
 
-  try:
-    msg = TextSendMessage(text=text)
-  except Exception as e:
-    msg = TextSendMessage(text=str(e))
-  try:
-    line_bot_api.reply_message(event.reply_token, msg)
-  except Exception as e:
-    print("error" + str(e))
+    try:
+        msg = TextSendMessage(text=text)
+    except Exception as e:
+        msg = TextSendMessage(text=str(e))
+    try:
+        line_bot_api.reply_message(event.reply_token, msg)
+    except Exception as e:
+        print("error" + str(e))
 
 
 # Audio message events
 @handler.add(MessageEvent, message=AudioMessage)
 def handle_audio_message(event):
 
-  user_id = event.source.user_id
-  msg_content = line_bot_api.get_message_content(event.message.id)
+    user_id = event.source.user_id
+    msg_content = line_bot_api.get_message_content(event.message.id)
 
-  #reply message
-  reply_arr = []
+    #reply message
+    reply_arr = []
 
-  msg = "Audio message successfully received! Start analyzing, please hold on..."
-  reply1 = TextSendMessage(text=msg)
-  reply_arr.append(reply1)
+    msg = "Audio message successfully received! Start analyzing, please hold on..."
+    reply1 = TextSendMessage(text=msg)
+    reply_arr.append(reply1)
 
-  dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
-  dt2 = dt1.astimezone(timezone(timedelta(hours=8)))  # 轉換時區 -> 東八區
-  CurrTime = dt2.strftime("%Y-%m-%d_%H:%M:%S")  # 將時間轉換為 string
+    dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
+    dt2 = dt1.astimezone(timezone(timedelta(hours=8)))  # 轉換時區 -> 東八區
+    CurrTime = dt2.strftime("%Y-%m-%d_%H:%M:%S")  # 將時間轉換為 string
 
-  #create m4a file
-  file_name = CurrTime + '.m4a'
+    #create m4a file
+    file_name = CurrTime + '.m4a'
 
-  with open(file_name, 'wb') as fd:
-    for chunk in msg_content.iter_content():
-      fd.write(chunk)
+    with open(file_name, 'wb') as fd:
+        for chunk in msg_content.iter_content():
+            fd.write(chunk)
 
-  try:
-    openai.api_key = os.environ['OPENAI_API_KEY']
+    try:
+        openai.api_key = os.environ['OPENAI_API_KEY']
 
-  except KeyError:
-    sys.stderr.write("""
+    except KeyError:
+        sys.stderr.write("""
         You haven't set up your API key yet.
 
         If you don't have an API key yet, visit:
@@ -101,65 +101,70 @@ def handle_audio_message(event):
 
         Then, open the Secrets Tool and add OPENAI_API_KEY as a secret.
         """)
-    exit(1)
+        exit(1)
 
-  client = OpenAI()
+    client = OpenAI()
 
-  #open m4a file
-  m4a_file = open(file_name, "rb")
-  transcript = client.audio.transcriptions.create(model="whisper-1",
+    #open m4a file
+    m4a_file = open(file_name, "rb")
+    transcript = client.audio.transcriptions.create(model="whisper-1",
                                                   file=m4a_file,
                                                   response_format="text")
 
-  path = 'output.txt'
-  f = open(path, 'w')
-  f.write(transcript)
-  f.close()
+    path = 'output.txt'
+    f = open(path, 'w')
+    f.write(transcript)
+    f.close()
 
-  #implement NLP
-  NLP = NLP_obj.NLP_features(path)
-  NLP.generate_features()
-  print(NLP.feature_table)
+    #implement NLP
+    NLP = NLP_obj.NLP_features(path)
+    NLP.generate_features()
+    print(NLP.feature_table)
+  
+    #write nlp result into csv
+    with open('nlp.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(NLP.feature_table)
 
-  # wav_filename = 'output.wav'
-  # sound = AudioSegment.from_file(m4a_file, format="m4a")
-  # sound.export(wav_filename, format='wav')
+    # wav_filename = 'output.wav'
+    # sound = AudioSegment.from_file(m4a_file, format="m4a")
+    # sound.export(wav_filename, format='wav')
 
-  # # # fh = open("Baycrest2103.wav", "rb")
-  # # file_path = wav_filename
+    # # # fh = open("Baycrest2103.wav", "rb")
+    # # file_path = wav_filename
 
-  # # # Read and rewrite the file with soundfile
-  # # data, samplerate = soundfile.read(file_path)
-  # # soundfile.write(file_path, data, samplerate)
+    # # # Read and rewrite the file with soundfile
+    # # data, samplerate = soundfile.read(file_path)
+    # # soundfile.write(file_path, data, samplerate)
 
-  # # # Now try to open the file with wave
-  # # with wave.open(file_path) as file:
-  # #     print('File opened!')
-  # command = f"ffmpeg -i {file_name} -ar 16000 {wav_filename}"
-  # subprocess.run(command, shell=True)
+    # # # Now try to open the file with wave
+    # # with wave.open(file_path) as file:
+    # #     print('File opened!')
+    # command = f"ffmpeg -i {file_name} -ar 16000 {wav_filename}"
+    # subprocess.run(command, shell=True)
 
-  #implement MFCC
-  # MFCC = mfcc_obj.mfcc_features(wav_filename)
-  # MFCC.mfcc_feature_calculation()
-  # MFCC.mfcc_output("testdoc")
+    #implement MFCC
+    # MFCC = mfcc_obj.mfcc_features(wav_filename)
+    # MFCC.mfcc_feature_calculation()
+    # MFCC.mfcc_output("testdoc")
 
-  msg = "the final result is..."
-  reply2 = TextSendMessage(text=msg)
-  reply_arr.append(reply2)
+    msg = "the final result is..."
+    reply2 = TextSendMessage(text=msg)
+    reply_arr.append(reply2)
 
-  line_bot_api.reply_message(event.reply_token, reply_arr)
+    line_bot_api.reply_message(event.reply_token, reply_arr)
 
 
 @app.route("/", methods=['GET'])
 def home():
 
-  reply_arr = []
-  # user_ids = line_bot_api.get_friend_ids()
-  Init_msg = "hi there! I'm AD diagnoser! Welcome to start trial with me!You can start with telling a story with the picture below to start the diagnosing process!"
+    reply_arr = []
+    # user_ids = line_bot_api.get_friend_ids()
+    Init_msg = "hi there! I'm AD diagnoser! Welcome to start trial with me!You can start with telling a story with the picture below to start the diagnosing process!"
 
-  img_URL = "https://i.imgur.com/U8ssZ3a.jpg"
-  text_message = TextSendMessage(text=Init_msg)
-  reply_arr.append(text_message)
+    img_URL = "https://i.imgur.com/U8ssZ3a.jpg"
+    text_message = TextSendMessage(text=Init_msg)
+    reply_arr.append(text_message)
 
   # image_message = ImageSendMessage(
   #     original_content_url=
@@ -168,9 +173,9 @@ def home():
   #     'https://ithelp.ithome.com.tw/storage/image/ironman13thsidebar.png')
   # reply_arr.append(image_message)
 
-  image_message = ImageSendMessage(original_content_url=img_URL,
+    image_message = ImageSendMessage(original_content_url=img_URL,
                                    preview_image_url=img_URL)
-  reply_arr.append(image_message)
+    reply_arr.append(image_message)
 
   # for user_id in user_ids:
   # line_bot_api.push_message(user_id, TextSendMessage(text=Init_msg))
@@ -178,9 +183,9 @@ def home():
   #       user_id,
   #       ImageSendMessage(original_content_url=img_URL,
   #                        preview_image_url=img_URL))
-  line_bot_api.push_message("Ub1e82ffd0b71b067b7196330dcff2aee", reply_arr)
+    line_bot_api.push_message("Ub1e82ffd0b71b067b7196330dcff2aee", reply_arr)
 
-  return "hi"
+    return "hi"
 
 
 if __name__ == "__main__":
